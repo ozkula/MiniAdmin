8bcbc3ca1f861a3e10dbabc4e3924570e83e9734
Rozaq.in <at.ozkula@gmail.com>
2015-09-16 10:22:30 +0700

    New CheckOut
    CI 301

---
+++ application/controllers/Login.php
@@ -0,0 +1,34 @@
+<?php 
+/**
+* 
+*/
+class Login extends CI_controller
+{
+	
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	function index(){
+		$data['error'] = 'Administrator Login';
+		$this->load->view('login',$data);
+	}
+	function check_login(){
+		$post = $this->input->post();
+		$username = $post['username'];
+		$password = $post['password'];
+		$login = $this->Mrun->check_login($username, $password);
+
+		if (!$login) {
+			$data['error'] = "Login Error Username or Password";
+			$this->load->view('login',$data);
+		} else {
+			session_start();
+			$this->session->set_userdata('login',$login);
+			redirect('dashboard');
+		}
+
+	}
+}
+?>
